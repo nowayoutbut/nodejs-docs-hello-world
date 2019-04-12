4e138e2f09bff5659dd237798b8f380f20b44d21
うらあっくん <nowayoutbut@outlook.jp>
2019-04-12 10:03:43 +0900

    test5

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
 
-app.get('/', (req, res) => res.send(`Hello World! Azure Express listening on PORT:${port}`))
+app.get('/', (req, res) => res.send(`Hello World! Azure Express listening on HOST:${host} PORT:${port}`))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on Host:${host} port ${port}!`))
 
